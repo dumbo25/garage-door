c108b0d50f3a3d765130cd59c3b8163f8d17d6b4
dumbo25 <dumbo25@users.noreply.github.com>
2020-11-30 15:50:49 -0600

    Update garage.sh
    Add sleep and pull down resistor to get more stable read from sensor pin

--- usr/local/bin/garage.sh
+++ usr/local/bin/garage.sh
@@ -3,15 +3,18 @@
 # enter a  crontab: */5 * * * * sudo /usr/local/bin/garage.sh
 # If open send an alert and write to syslog
 up=0
-door=$(/home/pi/wiringPi/gpio/gpio read 17)
+mobile='your-number'
+
+sensor=5
+gpio -g mode $sensor down
+sleep 1
 
 # get current time
 currTime=`date +%k%M`
 
-
 # get whether or not on vacation
 # I get emails when SQLITE_BUSY occurs. So, changed timeout from 1000 to 20000. Seems like recommended solution.
-vacation=$(sqlite3 -init <(echo .timeout 20000) /var/www/db/garage.db "SELECT value FROM status WHERE name = \"vacation\"";)
+vacation=$(sqlite3 -init <(echo .timeout 20000) /var/www/db/garagedoor.db "SELECT value FROM status WHERE name = \"vacation\"";)
 
 
 # # if on vacation
@@ -20,7 +23,7 @@ then
 	# and door is up
 	if [ "$door" -eq "$up" ]; then
 		logger rpiGarageOpener: Garage Door Open
-		echo "close the garage door" | mail -s "Garage Door Open and on vacation" 5127139980@txt.att.net
+		echo "close the garage door" | mail -s "Garage Door Open and on vacation" $mobile
         fi
 # if not on vacation and time is between 10pm and midnight
 elif [ "$vacation" == "no" ] 
@@ -30,7 +33,7 @@ then
                 if [ "$door" -eq "$up" ]
 	        then
                         logger rpiGarageOpener: Garage Door Open
-                        echo "close the garage door" | mail -s "Garage Door Open late at night" 5127139980@txt.att.net
+                        echo "close the garage door" | mail -s "Garage Door Open late at night" $mobile
                 fi
         # or, if time is less then 7:00am
         elif [ $currTime -gt 0 -a $currTime -lt 700 ]
@@ -38,7 +41,7 @@ then
                 if [ "$door" -eq "$up" ]
         	then
                         logger rpiGarageOpener: Garage Door Open
-                        echo "close the garage door" | mail -s "Garage Door Open to early" 5127139980@txt.att.net
+                        echo "close the garage door" | mail -s "Garage Door Open to early" $mobile
                 fi
         fi
 fi
