50a02a1010d8c726650ba5f785dedf2c54f0fd4d
dumbo25 <dumbo25@users.noreply.github.com>
2020-11-29 18:38:31 -0600

    Update getWebsite.sh
    updated repository links

--- home/pi/getWebsite.sh
+++ home/pi/getWebsite.sh
@@ -7,18 +7,19 @@ mv /var/www/html/index.html /var/www/html/index2.html
 
 # get the accordion menu
 cd /var/www/html
-wget "https://raw.githubusercontent.com/dumbo25/garageDoorOpener2/main/index.php"
+wget "https://raw.githubusercontent.com/dumbo25/garage-door/main/var/www/html/index.php"
+wget "https://raw.githubusercontent.com/dumbo25/garage-door/main/var/www/html/schedule.php"
 sudo chmod 774 *.php
 sudo chown root *.php
 
 mkdir /var/www/html/css
 cd /var/www/html/css
-wget "https://raw.githubusercontent.com/dumbo25/garageDoorOpener2/main/style.css"
+wget "https://raw.githubusercontent.com/dumbo25/garage-door/main/var/www/html/css/style.css"
 sudo chown root *.css
 
 sudo mkdir /var/www/html/img
 cd /var/www/html/img
-wget "https://github.com/dumbo25/garageDoorOpener2/blob/main/remote-background.png"
-wget "https://github.com/dumbo25/garageDoorOpener2/blob/main/menu-icon.png"
-wget "https://github.com/dumbo25/garageDoorOpener2/blob/main/raspberry-pi.png"
+wget "https://github.com/dumbo25/garage-door/blob/main/var/www/html/img/raspberry-pi.png"
+wget "https://github.com/dumbo25/garage-door/blob/main/var/www/html/img/menu-icon.png"
+wget "https://github.com/dumbo25/garage-door/blob/main/var/www/html/img/remote-background.png"
 
