f055d863c59ce8816fa48bf984acc95dc01d1832
dumbo25 <dumbo25@users.noreply.github.com>
2020-12-12 13:49:36 -0600

    Add files via upload

---
+++ home/pi/api/close/get.py
@@ -0,0 +1,5 @@
+global Data
+# closes garage door
+cmd = 'sudo python /usr/local/bin/close.py'
+os.system(cmd)
+Data = {}
---
+++ home/pi/api/open/get.backup.sh
@@ -0,0 +1,5 @@
+global Data
+# opens garage door
+cmd = 'sudo python /usr/local/bin/open.py'
+os.system(cmd)
+Data = {}
