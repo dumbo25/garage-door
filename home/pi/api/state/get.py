109a81d5dc909080b7325af028378031a1a83ff7
dumbo25 <dumbo25@users.noreply.github.com>
2020-12-12 17:57:07 -0600

    Update get.py

--- home/pi/api/state/get.py
+++ home/pi/api/state/get.py
@@ -1,5 +1,4 @@
 global Data
-# returns whether garage door is open or closed
-cmd = 'sudo python /usr/local/bin/close.py'
-os.system(cmd)
-Data = {}
+# closes garage door
+cmd = 'sudo python /usr/local/bin/state.py'
+Data = {"Garage Door": os.system(cmd)}
