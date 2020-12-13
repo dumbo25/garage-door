56a2519ec4508337db564606f1dcec383c0e87b8
dumbo25 <dumbo25@users.noreply.github.com>
2020-12-12 18:00:38 -0600

    Update getScripts2.sh
    added state.py

--- home/pi/getScripts2.sh
+++ home/pi/getScripts2.sh
@@ -5,11 +5,13 @@
 cd /usr/local/bin
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/usr/local/bin/close.py
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/usr/local/bin/open.py
+wget https://raw.githubusercontent.com/dumbo25/garage-door/main/usr/local/bin/state.py
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/usr/local/bin/toggle.py
 
 # make the sripts executable
 sudo chmod +x close.py
 sudo chmod +x open.py
+sudo chmod +x state.py
 sudo chmod +x toggle.py
 
 
