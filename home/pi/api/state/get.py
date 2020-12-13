90a69e775c65b0ac3107a7015e49782c7e42a1a0
dumbo25 <dumbo25@users.noreply.github.com>
2020-12-12 19:47:36 -0600

    Update get.py
    I didn't realize this could be a regular python program or module, I thought it was limited to a few lines of code. Instead of calling state.py, I merged the code in and was able to set the return string as "open" or "closed". Much better

--- home/pi/api/state/get.py
+++ home/pi/api/state/get.py
@@ -1,4 +1,22 @@
+# gets state of the garage door
+
+from time import sleep
+import RPi.GPIO as GPIO
+
 global Data
-# closes garage door
-cmd = 'sudo python /usr/local/bin/state.py'
-Data = {"Garage Door": os.system(cmd)}
+
+SENSOR = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SENSOR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# wait for sensor pin to reach steady state before reading
+sleep(1)
+
+open = GPIO.input(SENSOR)
+GPIO.cleanup()
+s = 'closed'
+if open == 0:
+    s = 'open'
+
+Data = {"Garage Door": s}
