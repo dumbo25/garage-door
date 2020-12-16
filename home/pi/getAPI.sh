03233fc473b4923bb2190319e621bf3219468af8
dumbo25 <dumbo25@users.noreply.github.com>
2020-12-15 18:14:55 -0600

    Update getAPI.sh

--- home/pi/getAPI.sh
+++ home/pi/getAPI.sh
@@ -10,7 +10,6 @@ cd ~/api/open/.
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/open/get.py
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/open/get.json
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/open/get.txt
-wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/open/get.backup.sh
 
 cd ~/api
 mkdir close
@@ -18,7 +17,6 @@ cd ~/api/close/.
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/close/get.py
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/close/get.json
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/close/get.txt
-wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/close/get.backup.sh
 
 cd ~/api
 mkdir toggle
@@ -26,7 +24,6 @@ cd ~/api/toggle/.
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/toggle/get.py
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/toggle/get.json
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/toggle/get.txt
-wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/toggle/get.backup.sh
 
 cd ~/api
 mkdir state
@@ -34,6 +31,3 @@ cd ~/api/state/.
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/state/get.py
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/state/get.json
 wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/state/get.txt
-wget https://raw.githubusercontent.com/dumbo25/garage-door/main/home/pi/api/state/get.backup.sh
-
-
