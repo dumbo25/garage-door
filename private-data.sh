3f30131b0147759a3d6db8285a1d5bcc92359276
dumbo25 <dumbo25@users.noreply.github.com>
2020-12-27 11:12:36 -0600

    Create private-data.sh

---
+++ private-data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#  this file includes private-data that should not be published or shared
+
+#   **** DO NOT PUBLISH ITEMS BELOW ****
+#
+#   Replace values and angle brackets below with your values
+#
+COUNTRY="<your-two-letter-country-code>"
+STATE="<your-state-or-province>"
+CITY="<your-city>"
+LASTNAME="<your-company-or-lastname>"
+FIRSTNAME="<your-business-unit-or-firstname>"
+EMAIL="<your-throw-away-gmail-account>@gmail.com"
+CERTPASSWORD="<your-throw-away-gmail-password>"
+
+#
+#   Replace values above in angle brackets with your values
+#
+#   **** DO NOT PUBLISH ITEMS ABOVE ****
