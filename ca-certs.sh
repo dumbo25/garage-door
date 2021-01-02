93196f291a0238251147e97db22e66f6d4f6eb1a
dumbo25 <dumbo25@users.noreply.github.com>
2021-01-02 07:56:44 -0600

    Update ca-certs.sh
    simplified and moved more to private-data.sh

--- ca-certs.sh
+++ ca-certs.sh
@@ -5,44 +5,18 @@
 # run as:
 #   sudo bash ca-certs.sh
 
-# Global names
-#
+# exit on error
+set -e
+
 # defines values for private data that should not be shared
 source private-data.sh
-#
-# Directories and paths
-#
-HOMEDIR="/home/pi"
-CERTSDIR="/etc/ssl"
-ESC_CERTSDIR="\/etc\/ssl"
-#
-#   Key and pem paths and files used in openssl.cnf
-#
-PRIVATEPATH="$CERTSDIR/private/"
-CERTKEY="cakey.pem"
-KEYFILE="$PRIVATEPATH$CERTKEY"
-#
-CERTPATH="$CERTSDIR/certs/"
-CERTPEM="cacert.pem"
-CERTFILE="$CERTPATH$CERTPEM"
-#
-CONFFILE="$CERTSDIR/openssl.cnf"
-#
-#   The Common Name for the CA cannot be the same as the server or client (i.e., CA != FQDN)
-#
-FQDN="$HOSTNAME"
-CA="cacert"
-#
-#   Most browsers limit certs to 365 days or less
-#
-DAYS="365"
-#
-# End Global names
 
+CACERTFILE="$MYCERTSDIR/$CACRT.pem"
+CAKEYFILE="$MYCERTSDIR/$CAKEY.pem"
 
-echo "Starting Certificate Authority script"
-echo
+CADAYS=3650
 
+echo "Starting Certificate Authority Script"
 
 # The script must run as sudo. If not sudo, then exit
 #
@@ -54,15 +28,13 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # must edit private data before runnning the script
-if [[ "$CERTPASSWORD" == *"<"* ]]; then
+if [[ "$EMAIL" == *"<"* ]]; then
     echo
     echo "Please edit the global names in private-data.sh before running"
     echo "    Location = $CITY, $STATE, $COUNTRY"
     echo "    Name = $LASTNAME, $FIRSTNAME"
     echo "    email = $EMAIL"
     echo "    cert password = $CERTPASSWORD"
-    echo "    FQDN = $FQDN"
-    echo "    CA = $CA"
     echo
     echo " Feilure: Exiting Certificate Authority setup script"
 fi
@@ -72,12 +44,12 @@ fi
 # If the ca-cert key and pem files exist then prompt user if they really want
 # to continue. If they don't, then exit
 #
-if [ -f "$CERTFILE" ]; then
-    echo "CA cert file ($CERTFILE) exists."
-    if [ -f "$KEYFILE" ]; then
-        echo "CA key file ($KEYFILE) exists."
+if [ -f "$CACERTFILE" ]; then
+    echo "CA cert file ($CACERTFILE) exists."
+    if [ -f "$CAKEYFILE" ]; then
+        echo "CA key file ($CAKEYFILE) exists."
         echo
-        read -p "The CA files were created. Do you want to continue? " -n 1 -r
+        read -p "The CA files were created. Do you want to continue? " -r
         if [[ ! $REPLY =~ ^[Yy]$ ]]; then
             echo
             echo "Failure: Exiting Certificate Authority setup script"
@@ -88,129 +60,18 @@ else
     echo "No CA files found."
 fi
 
+# check if directory exists. If not make it. Move into it
+[ -d $MYCERTSDIR ] && echo "    $MYCERTSDIR directory exists" || mkdir $MYCERTSDIR
+cd $MYCERTSDIR
 
-# Make backups of each file, in case they need to be restored
-#
-# This should only be done one time - don't make backups after changes have
-# been made
-#
+# outputs:
+#   ca-crt.pem  ca-key.pem
 echo
-echo "Moving original files to *.backup in case they need to be recovered"
-if [ -f "$KEYFILE" ]; then
-    KEYBACKUP="$KEYFILE.backup"
-    if [ ! -f "$KEYBACKUP" ]; then
-        mv $KEYFILE $KEYBACKUP
-    fi
-fi
-
-if [ -f "$CERTFILE" ]; then
-    CERTBACKUP="$CERTFILE.backup"
-    if [ ! -f "$CERTBACKUP" ]; then
-        mv $CERTFILE $CERTBACKUP
-    fi
-fi
-
-if [ -f "$CONFFILE" ]; then
-    CONFBACKUP="$CONFFILE.backup"
-    if [ ! -f "$CONFBACKUP" ]; then
-        cp $CONFFILE $CONFBACKUP
-    fi
-fi
-
-
-# Change to certs directory and create sub directories
-#
-echo
-echo "Creating directories and moving into certs directory"
-cd $CERTDIR
-echo "    certs directory:"
-pwd
-
-[ -d $CACERTSDIR ] && echo "    $CACERTSDIR directory exists" || mkdir $CACERTSDIR
-cd $CERTSDIR
-pwd
-[ -d certs ] && echo "    certs directory exists" || mkdir certs
-[ -d crl ] && echo "    crl directory exists" || mkdir crl
-[ -d newcerts ] && echo "    newcerts directory exists" || mkdir newcerts
-[ -d private ] && echo "    private directory exists" || mkdir private
-sudo chown root:root private
-sudo chmod 700 private
-[ -f index.txt ] && echo "    index.txt exists" || rm index.txt
-touch index.txt
-[ -f serial ] && echo "    serial exists" || echo 01 > serial
-[ -f crlnumber ] && echo "    crlnumber exists" || echo 01 > crlnumber
-
-# Edit configuration file
-#   Made backup above
-#
-# need to add _default where it is missing (only state and organization have defaults)
-#
-# from beginning through [ CA_default ] no changes
-# [ CA_default ] take the defaults
-sed -i "s/.*demoCA.*/dir = $ESC_CERTSDIR/" $CONFFILE
-#     x509 extensions are defined in openssl.cnf under the section usr_cert
-# [ req ] no changes
-# [ req_distinguished_name ]
-sed -i "s/countryName_default.*= AU.*/countryName_default = $COUNTRY/" $CONFFILE
-sed -i "s/stateOrProvinceName_default.*= Some.*/stateOrProvinceName_default = $STATE/" $CONFFILE
-sed -i "/localityName.*= Locality.*/a localityName_default = $CITY" $CONFFILE
-sed -i "s/0.organizationName_default.*= Internet.*/0.organizationName_default = $LASTNAME/" $CONFFILE
-sed -i "/organizationalUnitName.*= Organizational/a organizationalUnitName_default = $FIRSTNAME" $CONFFILE
-sed -i "/commonName.*= Common Name.*/a commonName_default = $CA" $CONFFILE
-sed -i "/emailAddress.*= Email Address.*/a emailAddress_default = $EMAIL" $CONFFILE
-sed -i "/challengePassword.*= A challenge/a challengePassword_default = $CERTPASSWORD" $CONFFILE
-# [ usr_cert ]
-#   CA must have CA:TRUE and pathlen=0
-#   A 0 pathlen means the CA can only sign end user certificates and not CAs
-sed -i "s/basicConstraints=CA:FALSE/basicConstraints=CA:TRUE, pathlen=0/" $CONFFILE
-# [ v3_req ] - no changes
-# [ v3_ca ]
-sed -i "s/# keyUsage = cRLSign, keyCertSign/keyUsage= critical, cRLSign, digitalSignature, keyCertSign/" $CONFFILE
-# [ crl ext ]
-# [ proxy_cert_ext ]
-
-#   I am unsure how to complete subjectAltName for NAT'd servers with no DNS
-#   I'd prefer to use IP addresses
-#
-#subjectAltName          = @alt_ca
-#[alt_names]
-#IP.1 = 192.168.1.227
-#EOF
-
-# Generate Certificate Authority (CA)
-#
-#   Before creating server/client certificate, setup a self-signed Certificate Authority (CA),
-#   which can be used to sign the server/client certificates. Once created, the CA cert will
-#   act as the trusted authority for both your server and client certificates (or certs).
-#
-echo
-echo "Generate Certificate Authority (CA)"
-echo
-
-
-# Generate Certificate Authority private key (cakey.pem) and cert (cacert.pem)
-#
-#     openssl req = request command (openssl req -help)
-#     -new = new request
-#     -x509 = output x509 structure
-#     -nodes = don't encrypt the output key
-#     -sha256 = encryption
-#     -days = number of days the cert is valid
-#     -config = request templayte file
-#     -keyform PEM = human readable form
-#     -keyout = CA key
-#     -outform PEM = human readbale form
-#     -out = CA cert
-openssl req -config $CONFFILE -new -x509 -days $DAYS -keyform PEM -keyout $KEYFILE -outform PEM -out $CERTFILE
-
-# *** need to verify CA
-echo
-echo "Verify CA certificate"
-openssl x509 -noout -text -in $CERTFILE
-
-echo "Verify CA Key:"
-openssl rsa -in $KEYFILE -check
+echo "Generate Certtificate Authority cert and key"
+echo "   prompts for PEM Pass Phrase"
+echo "$ openssl req -new -x509 -days $CADAYS -keyout $CAKEYFILE -out $CACERTFILE -passout 'pass:$PEMPSWD' -subj \"/CN=$CANAME/emailAddress=$EMAIL/C=US/ST=$STATE/L=$CITY/O=$LASTNAME/OU=$FIRSTNAME\""
+openssl req -new -x509 -days $CADAYS -keyout $CAKEYFILE -out $CACERTFILE -passout 'pass:$PEMPSWD' -subj "/CN=$CANAME/emailAddress=$EMAIL/C=US/ST=$STATE/L=$CITY/O=$LASTNAME/OU=$FIRSTNAME"
 
 echo
-echo "Exiting Certificate Authority Script"
+echo "SUCCESS: Exiting Certificate Authority Script"
 
